ad01ef879fbb858473cd031c5e8ee5f303a953d0
ion <ion2005@mail.ru>
2024-10-05 18:23:26 +0300

    init

---
+++ analytics-builder-service/src/main/java/ru/tomatojAI/analytics_builder_service/AnalyticsBuilderServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.tomatojAI.analytics_builder_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsBuilderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsBuilderServiceApplication.class, args);
+	}
+
+}

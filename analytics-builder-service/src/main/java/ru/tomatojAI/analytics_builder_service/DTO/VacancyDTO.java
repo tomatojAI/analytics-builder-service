39f91694f747a2ef829f8e074d6991f8c75ec618
ion <ion2005@mail.ru>
2024-10-08 00:50:04 +0300

    add VacancyDTO

---
+++ analytics-builder-service/src/main/java/ru/tomatojAI/analytics_builder_service/DTO/VacancyDTO.java
@@ -0,0 +1,11 @@
+package ru.tomatojAI.analytics_builder_service.DTO;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VacancyDTO {
+    private String name;
+    private Long salary;
+}
